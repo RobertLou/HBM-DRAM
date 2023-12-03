0aa3d46cc5ccd0686ea08fa4ec32f6fef5787c40
RobertLou <2874395462@qq.com>
2023-12-03 21:01:40 +0800

    [Test] Add test for analyzing atomicAdd to a pinned memory place

--- test/atomic_add_host_test.cu
+++ test/atomic_add_host_test.cu
@@ -1,44 +1,46 @@
 #include <iostream>
-#include <thread>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ void add(int *global_counter, int N){
+__global__ void add1(int *global_counter, int N){
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     if(i < N){
         atomicAdd(global_counter, 1);
     }
 }
 
-void Add(int *global_counter, int N) {
-    add<<<(N + 127) / 128, 128>>>(global_counter, N);
+__global__ void add2(int *global_counter, int N){
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if(i < N){
+        atomicAdd(global_counter, 1);
+    }
 }
 
-
 int main(){
-
     int *h_global_counter, *d_global_counter;
+    int *host_counter;
     int size = 10000;
 
     h_global_counter = (int *)malloc(sizeof(int));
     cudaMalloc((void**)&d_global_counter, sizeof(int));
-
+    cudaHostAlloc((void **)&host_counter, sizeof(int), cudaHostAllocDefault);
+    
     *h_global_counter = 0;
+    *host_counter = 0;
     cudaMemcpy(d_global_counter, h_global_counter, sizeof(int), cudaMemcpyHostToDevice);
 
-    std::thread thread1(Add, d_global_counter, size);
-    std::thread thread2(Add, d_global_counter, size);
-    std::thread thread3(Add, d_global_counter, size);
-    std::thread thread4(Add, d_global_counter, size);
+    for(int i = 0;i < 1000;i++){
+        add1<<<(size + 127) / 128, 128>>>(d_global_counter, size);
+    }
 
-    thread1.join();
-    thread2.join();
-    thread3.join();
-    thread4.join();
+    for(int i = 0;i < 1000;i++){
+        add2<<<(size + 127) / 128, 128>>>(host_counter, size);
+    }
 
     cudaDeviceSynchronize();
     cudaMemcpy(h_global_counter, d_global_counter, sizeof(int), cudaMemcpyDeviceToHost);
 
     std::cout << *h_global_counter << std::endl;
+    std::cout << *host_counter << std::endl;
     return 0;
 }
